f90ac49381e450df40620fa1c89d0a9187c4b4e5
pishivam009 <pishivam009@gmail.com>
2021-11-14 14:29:11 +0530

    2. Max Min Of Array

---
+++ src/com/piyush/code/MaxMinOfArray.java
@@ -0,0 +1,22 @@
+package com.piyush.code;
+
+public class MaxMinOfArray {
+
+	public static int[] way1(int[] a) {
+		int max=a[0];
+		int min=a[0];
+		
+		for(int i=0; i<a.length;i++) {
+			if(a[i]>max) max=a[i];
+			if(a[i]<min) min=a[i];
+		}
+		
+		return new int[]{max,min};
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a=way1(new int[] {3,5,9,7,8,3,1,4,5});
+		System.out.print("Max="+a[0]+" Min="+a[1]);
+	}
+
+}
