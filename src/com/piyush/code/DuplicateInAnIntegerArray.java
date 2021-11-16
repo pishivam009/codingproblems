6f80bbfce160ba9df3be14864f41d37f543a60f2
pishivam009 <pishivam009@gmail.com>
2021-11-16 10:07:20 +0530

    11. Duplicate in an integer array of n+1 integers

---
+++ src/com/piyush/code/DuplicateInAnIntegerArray.java
@@ -0,0 +1,35 @@
+package com.piyush.code;
+
+import java.util.Arrays;
+
+public class DuplicateInAnIntegerArray {
+
+	public static int way1(int[] a) {
+		int[] b = new int[a.length + 1];
+
+		for (int k : a) {
+
+			if (b[k] == 0)
+				b[k] = k;
+
+			else
+				return k;
+		}
+		return -1;
+	}
+	
+	public static int way2(int[]a) {
+		Arrays.sort(a);
+		for(int i=1; i<a.length;i++) {
+			if(a[i]==a[i-1]) return a[i];
+		}
+		
+		return -1;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(way2(new int[] { 7, 9, 7, 4, 2, 8, 7, 7, 1, 5 }));
+	}
+
+}
