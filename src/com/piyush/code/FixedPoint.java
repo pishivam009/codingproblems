4214b6c6918ef20de658ccbde1f266d744de2a4f
pishivam009 <pishivam009@gmail.com>
2021-12-06 15:20:18 +0530

    91. Find a Fixed Point (Value equal to index) in a given array

---
+++ src/com/piyush/code/FixedPoint.java
@@ -0,0 +1,15 @@
+package com.piyush.code;
+
+public class FixedPoint {
+
+	public static void way1(int[] a) {
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] == i + 1)
+				System.out.println(a[i]);
+		}
+	}
+
+	public static void main(String[] args) {
+		way1(new int[] { 15, 2, 45, 12, 7 });
+	}
+}
